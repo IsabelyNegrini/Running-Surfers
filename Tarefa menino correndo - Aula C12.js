bdc1b2789db2945b9f6e39e1a1d6c0957de8c3c7
IsabelyNegrini <126030036+IsabelyNegrini@users.noreply.github.com>
2023-03-06 19:26:49 -0300

    Add files via upload

---
+++ Tarefa menino correndo - Aula C12.js
@@ -0,0 +1,61 @@
+var path, boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+//loadImage (carregarImagem) da pista)
+pathImg = loadImage("path.png");
+ //loadAnimation (carregarAnimação) de corrida para o menino
+boyImg = loadAnimation ("Runner-1.png", "Runner-2.png");
+ 
+}
+
+function setup(){
+  
+createCanvas(400,400);
+//crie um sprite para a pista 
+path = createSprite (190, 400)
+path.addImage ("path", pathImg);
+//Faça com que a pista seja um fundo que se move dando a ela velocity Y.
+path.y = path.width /4;
+
+path.scale=1.2;
+
+//crie um sprite de menino
+boy=createSprite (50,200,200);
+boy.addAnimation("boy", boyImg);
+boy.scale=.06;
+  
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+//defina visibilidade como falsa para o limite à esquerda
+leftBoundary.visible=false;
+
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+//defina visibilidade como falsa para o limite à direita
+rightBoundary.visible=false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  // mover o menino com o mouse usando mouseX
+  boy.x=World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+
+  // colidir o menino com os limites invisíveis da esquerda e da direita
+  boy.collide(rightBoundary);
+  boy.collide(leftBoundary);
+
+  
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/4;
+  }
+  
+  drawSprites();
+}
